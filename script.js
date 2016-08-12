464e76fd343619c7d79a550ddafaa6c1626c22d9
mindmatter <dev@mindmatter.co>
2016-08-12 21:03:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   $("#login").submit(function() {
-    $("#message").fadeIn("slow", function() {
-      $(this).fadeOut("slow");
-    });
+    $("#message").show();
   });
 }
