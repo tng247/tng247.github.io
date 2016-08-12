2b17f8ebc6740a7a58d5420d0dd066f66106935e
mindmatter <dev@mindmatter.co>
2016-08-12 21:27:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("#login").submit(function () {
-    $("#message").removeClass("ping").addClass("ping");
+    alert("Invalid account details, please try again.");
   });
 });
