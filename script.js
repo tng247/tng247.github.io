7179b02820c592ad4fb7ae5c33a5f15ab89070cc
mindmatter <dev@mindmatter.co>
2016-08-13 14:10:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
   var failure = "Invalid details, please try again.";
   var success = "Welcome back, connecting you now...";
 
-  $("#submit").html(failure);
+  $("#signin").html(failure);
 
   $("#login").submit(function (e) {
     e.preventDefault();
 //    alert(failure);
-    location.assign("/#submit");
+    location.assign("/#signin");
     location.reload(true);
   });
 });
