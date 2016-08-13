380f84cd872e1f7c5c81f4a5805f39137437112f
mindmatter <dev@mindmatter.co>
2016-08-13 12:47:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
+  var HTML = "<p class=\"message\">Invalid account details, please try again.</p>";
+
   $("#login").submit(function () {
-    alert("Invalid account details, please try again.");
+//    alert("Invalid account details, please try again.");
+    $("#submit").after(HTML);
   });
 });
